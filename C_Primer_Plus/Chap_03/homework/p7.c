ae6f4c92527544220eb4bb00b1366e8e816c77a2
zhangyu <zhangyu2@tellhow.com>
2023-11-10 22:07:16 +0800

    Chao03 completed

--- C_Primer_Plus/Chap_03/homework/p7.c
+++ C_Primer_Plus/Chap_03/homework/p7.c
@@ -2,7 +2,11 @@
 
 int main(void)
 {
-  
+  float inches;
+  printf("please enter your height in inches:");
+  scanf("%f",&inches);
+
+  printf("Your height in cm is %f\n", inches * 2.54);
 
   return 0;
 }
