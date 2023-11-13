b7d671b9e394d86ca74b72c8a3ee741d5a396bbf
zhangyu <zhangyu2@tellhow.com>
2023-11-13 21:44:43 +0800

    committed

---
+++ C_Primer_Plus/Chap_04/longstring.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+  printf("here's one way to print a");
+  printf("long string.\n");
+
+  printf("Here's one way to print a \
+long string.\n");
+
+  printf("Here's one way to print a long string.\n");
+
+  return 0;
+}
