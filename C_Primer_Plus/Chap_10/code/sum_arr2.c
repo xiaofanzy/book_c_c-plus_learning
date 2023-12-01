5d0e0bfe330b44f04009771a2eb54ae13dffe7e3
zhangxiaofan <15755156769@163.com>
2023-12-01 17:57:22 +0800

    code

---
+++ C_Primer_Plus/Chap_10/code/sum_arr2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#define SIZE 10
+
+int sum(int * start,int * end);
+
+int main(void)
+{
+  int marbles[SIZE] = {20,10,5,39,4,16,19,26,31,20};
+  int answer;
+
+  answer = sum(marbles,marbles + SIZE);
+
+  printf("The total number of marbles is %d\n",answer);
+
+  return 0;
+}
+
+int sum(int * start,int * end)
+{
+  int total = 0;
+  
+ while(start < end)
+	total += *start++;
+
+  return total;
+  
+}
