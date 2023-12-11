643187c9beb229f4bc7301955af5a851d68b7f05
zhangyu <zhangyu2@tellhow.com>
2023-12-11 16:12:25 +0800

    changes for commit

--- C_Primer_Plus/Chap_10/code/zippo.c
+++ C_Primer_Plus/Chap_10/code/zippo.c
@@ -7,5 +7,12 @@ int main(void)
   printf("zippo = %p, zipp0 + 1 = %p.\n",zippo, zippo + 1);
   printf("zippo[0] = %p, zipp[0] + 1 = %p.\n",zippo[0], zippo[0] + 1);
   printf("*zippo = %p, *zippo + 1 = %p.\n",*zippo, *zippo + 1);
+  printf("zippo[0][0] = %d.\n",zippo[0][0]);
+  printf("*zippo[0] = %d.\n",*zippo[0]);
+  printf("**zippo = %d\n",**zippo);
+  // 这里不能使用*(*zippo[2] + 1) 因为zippo[2] 本身就是地址， 地址便宜一个单位量还是地址，
+  // 我们取值的时候，要求这个地址指向的值是int类型的，但是很明显这个里面指向的值是个地址值，而非一个int类型的值；
+  printf("*(*zippo[2] + 1) = %d\n",*(zippo[2] + 1));
+
   return 0;
 }
