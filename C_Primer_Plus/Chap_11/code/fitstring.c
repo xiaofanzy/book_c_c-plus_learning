5643d78274a5cf22b85327521fc4c6a8d073cf66
zhangyu <zhangyu2@tellhow.com>
2023-12-12 17:47:33 +0800

    code

--- C_Primer_Plus/Chap_11/code/fitstring.c
+++ C_Primer_Plus/Chap_11/code/fitstring.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
+#include <string.h>
+
+void fit(char * m,unsigned int size);
 
 int main(void)
 {
+  char mesg[] = "hold on to you hats,hackers. ";
 
+  puts(mesg);
+  fit(mesg, 7);
+  puts(mesg);
+  puts(mesg + 8);
+  
+
+  return 0;
+}
+
+//我要改那玩意，所以不能用const 限制死
+void fit(char * m,unsigned int size)
+{
+  if (strlen(m) > size)
+  {
+    *(m + size) = '\0';
+  }
   
 }
