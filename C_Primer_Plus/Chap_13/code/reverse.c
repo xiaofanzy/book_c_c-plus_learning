9ae033ef42b98a4c0772d510b085dfe98ceed873
zhangxiaofan <15755156769@163.com>
2023-12-20 18:47:47 +0800

    code

---
+++ C_Primer_Plus/Chap_13/code/reverse.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 50
+
+int main(void)
+{
+  char file[SIZE];
+  FILE *fp;
+  char ch;
+
+  puts("Enter the name of file to be processed:");
+  gets(file);
+
+  if ((fp = fopen(file,"r")) != NULL)
+  {
+    printf("reverse can't open %s.\n", file);
+		exit(1);
+  }
+
+  fseek(fp,0L,SEEK_END);
+  int len = ftell(fp);
+
+  for (int i = 1; i <= len; i++)
+  {
+    fseek(fp,-i,SEEK_END);
+    ch = getc(fp);
+    putchar(ch);
+  }
+
+  putchar('\n');
+  
+  
+
+  
+
+  return 0;
+}
