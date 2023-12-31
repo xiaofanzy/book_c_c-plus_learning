23da5093350683991fdfeff090acc49aaba59e79
zhangyu <zhangyu2@tellhow.com>
2023-12-31 19:18:11 +0800

    code up

--- C++_Primer_Plus/Chap_03/homework/r3.cpp
+++ C++_Primer_Plus/Chap_03/homework/r3.cpp
@@ -4,7 +4,25 @@ int main(void)
 {
   using namespace std;
 
-  cout << "E"
+  cout << "enter a latitude in degrees, minutes, and seconds:" << endl;
+  cout << "First, enter the degrees: ";
+  int degrees;
+  cin >> degrees;
+  cout << "Next, enter the minutes of arc: ";
+  int minutes;
+  cin >> minutes;
+  cout << "Finally, enter the seconds of arc: ";
+  int seconds;
+  cin >> seconds;
+
+  const int kSecondsPerMinute = 60;
+  const int kMinutesPerDegree = 60;
+  const int kSecondsPerDegree = kSecondsPerMinute * kMinutesPerDegree;
+
+  double total_seconds = seconds + minutes * kSecondsPerMinute + degrees * kSecondsPerDegree;
+  double total_degrees = total_seconds / kSecondsPerDegree;
+
+  cout << degrees << " degrees, " << minutes << " minutes, " << seconds << " seconds = " << total_degrees << " degrees" << endl;
 
   return 0;
 }
