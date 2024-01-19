0fa00d88f510612415353d30f0701742f2a2a545
zhangyu <zhangyu2@tellhow.com>
2024-01-19 16:23:30 +0800

    copilot code update beacuse of the view is too long

--- C++_Primer_Plus/Chap_06/homework/p3.cpp
+++ C++_Primer_Plus/Chap_06/homework/p3.cpp
@@ -6,7 +6,35 @@ void showmenu();
 
 int main(void)
 {
+  char choose;
   showmenu();
+
+  cout << "Please enter a c, p, t, or g: ";
+  cin.get(choose);
+
+  while(choose != 'f')
+    {
+      switch(choose)
+        {
+          case 'c':
+            cout << "A maple is a carnivore.\n";
+            break;
+          case 'p':
+            cout << "A maple is a pianist.\n";
+            break;
+          case 't':
+            cout << "A maple is a tree.\n";
+            break;
+          case 'g':
+            cout << "A maple is a game.\n";
+            break ;
+          default:  
+            cout << "Please enter a c, p, t, or g: ";
+            break;
+        }
+      cin.get(choose);
+    }
+  
 
 
 
@@ -16,5 +44,8 @@ int main(void)
 
 void showmenu(void)
 {
-  cout << "";
+  cout << "Please enter one of the following choices:\n"
+       << "c) carnivore\t\tp) pianist\n"
+       << "t) tree\t\t\tg) game\n"
+        << "f) quit\n";
 }
