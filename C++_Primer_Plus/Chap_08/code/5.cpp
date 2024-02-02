9ee39d0921a8b7a7f6affd4e81716695d481af67
zhangxiaofan <15755156769@163.com>
2024-02-02 09:56:04 +0800

    code

--- C++_Primer_Plus/Chap_08/code/5.cpp
+++ C++_Primer_Plus/Chap_08/code/5.cpp
@@ -1,25 +1,29 @@
 #include <iostream>
 
 using namespace std;
+
 double cube(double a);
-double recube(double &a);
+double recube(const double &ra);
+
 int main(void)
 {
-  double x = 3.0;
-  cout << cube(x) << " is " << x << endl;
-  cout << recube(x) << " is " << x << endl;
+	double x = 3.0;
+	const double y = 5.0;
 
-  return 0;
+	cout << cube(5.0 + x) << " = cube of " << "5.0 + x" << endl;
+	
+	cout << recube(y) << " = cube of " << y << endl;
+
+	return 0;
 }
 
 double cube(double a)
 {
-  a *= a * a;
-  return a;
+	a *= a * a;
+	return a;
 }
 
-double recube(double &a)
+double recube(const double &ra)
 {
-  a *= a * a;
-  return a;
+	return ra * ra * ra;
 }
